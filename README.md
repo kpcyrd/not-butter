7f449639e212fb155ebd34371dc0f67a97937720
kpcyrd <git@rxv.cc>
2016-11-30 01:24:41 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# not butter
+
+```
+var butter = require('not-butter');
+if(!butter) {
+    ...
+}
+```
+
+## Install
+
+```
+npm install not-butter
+```
+
+## License
+
+Microsoft Public License (MS-PL)
---
+++ butter.js
@@ -0,0 +1,75 @@
+// QmZzQDki6px3Li7fDxXAqUbVA8zyu44vo9RRgsYXffvHfP butter/
+
+module.exports = exports = [
+    {alt: 'I can\'t believe it\'s not butter',
+        src: 'https://i.imgur.com/hOAjpsY.png',
+        ipfs: 'QmSzP7UgVpf21vcWE1VKJGJPEz4kzpKUsF7wNWKV8S4pQz'},
+
+    {alt: 'I totally thought it was butter',
+        src: 'https://i.imgur.com/FZLQPkE.jpg',
+        ipfs: 'QmeQuBSwuyerU7sVDu5aTrJVzaRyZjqp6rdYSbRkCYkLrC'},
+
+    {alt: 'you were laboring under the misapprehension that this is butter',
+        src: 'https://i.imgur.com/mrJZCOt.jpg',
+        ipfs: 'QmRqKHzKyjcghC41Gg5qZQMVGQNtEU8hfPRppDuQJ26Ust'},
+
+    {alt: 'butter it\'s not',
+        src: 'https://i.imgur.com/53mdDxk.jpg',
+        ipfs: 'QmcC1NYLNq52Z5tMmXGeDchAP9nsbtno7obLvAFpH5vvu9'},
+
+    {alt: 'could it be butter',
+        src: 'https://i.imgur.com/ClNTq0J.jpg',
+        ipfs: 'QmaDsvBx8HEgr2yuEP4tLAgSoaxi3QWHDRmy1Xoc2WMqSs'},
+
+    {alt: 'you\'d think it\'s butter',
+        src: 'https://i.imgur.com/nXZ3yEM.jpg',
+        ipfs: 'QmT5317m1C6HMUwJ5gqMyDvP2TAcEiM1mijZBbgpPygkHr'},
+
+    {alt: 'you\'d butter believe it',
+        src: 'https://i.imgur.com/DWGlu3k.jpg',
+        ipfs: 'QmPVyyLcnmCYXxGepwmLAZP36z4MA1oqrCgLMW9Y4N3CWH'},
+
+    {alt: 'what, not butter',
+        src: 'https://i.imgur.com/VnVqxVB.jpg',
+        ipfs: 'QmWVf4hQXTb6x31Tnn93G8puq7VEADmX3zYHNtvrLgXRMz'},
+
+    {alt: 'this is not butter it\'s butteriffic',
+        src: 'https://i.imgur.com/VWbSS5u.jpg',
+        ipfs: 'QmYpNbZfuCELmj7MNdtY5F84FRSH2juQa6jHKjarZgRz6w'},
+
+    {alt: 'taste like butter',
+        src: 'https://i.imgur.com/JNBTmdp.jpg',
+        ipfs: 'QmXrsXGD1bB7bD4NyrZ6bM83yTiWYzgCMvSJq8s6P6KMSk'},
+
+    {alt: 'is it butter?',
+        src: 'https://i.imgur.com/6ZQMCgK.jpg',
+        ipfs: 'QmU75FUejQ8JGKepsDzRoQG2dsGJW1kgYD3WJQRBP54Pgg'},
+
+    {alt: 'who needs butter',
+        src: 'https://i.imgur.com/aGRvJPg.jpg',
+        ipfs: 'QmQqEdzW6dQepUtLMpTnPuHaaKDEGTiUquv7CLq5meMnbL'},
+
+    {alt: 'the big buttery one',
+        src: 'https://i.imgur.com/I5ky5lX.jpg',
+        ipfs: 'QmNq6CY9jpKVFux6zvAqNQkP4HiYTU9vZeyaZfEWbwoyiM'},
+
+    {alt: 'utterly butterly',
+        src: 'https://i.imgur.com/LviB94H.jpg',
+        ipfs: 'QmcdiQH9kc7CJ19RjENuCZGaopeiwgdzeaqJCpZJ4VGcrD'},
+
+    {alt: 'no ifs ands or butter',
+        src: 'https://i.imgur.com/sGqKLLe.jpg',
+        ipfs: 'QmNfUgakNwY6h4VgRmRjchp6UTCGsCBQSofpDQsZeg7UV5'},
+
+    {alt: 'isn\'t it butter',
+        src: 'https://i.imgur.com/xua9955.jpg',
+        ipfs: 'QmZeoT2snVFKxb1UM89VniqJM5GrwUW4f7RQNdBR1KkZR8'},
+
+    {alt: 'unbelieveable this is not butter',
+        src: 'https://i.imgur.com/6sjq2jF.jpg',
+        ipfs: 'QmZjt5qq7GFXUzbA4i8VFb3Sa51Q58jkF274jxkfTw3nbE'},
+
+    {alt: 'memories of butter',
+        src: 'https://i.imgur.com/8jMnDOD.jpg',
+        ipfs: 'QmVV1GTEK9CksgKfZTPLFSRHtAMz4KrjewiUB2RmgcdSRQ'},
+];
